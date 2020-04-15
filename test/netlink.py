5c184f3149804579932c0ce860e0ed35c724d121
Steffen Christgau <mail@s14u.de>
2020-04-15 20:49:08 +0200

    feat(src): observe network address changes
    Instead of determining the network addresses in use at startup and
    assume them to be static over the whole runtime, addresses and changes
    to them are now tracked via the introduced NetworkAddressMonitor class.
    Consequently, a network interface can be specified which does not have
    useable addresses yet. If such one is assigned at a later point in time,
    the desired service type (WSD client or host) is started. This has two
    benefits. First, a change in networks (in case of a WiFi, e.g.) makes
    the host to be announced automatically and no restart of the application
    is required. Second, temporary/tentative addresses are not used for
    binding and wsdd waits for those to become ready for use. This is
    especially required for IPv6 tentative addresses present, e.g., at boot
    time due to Duplication Address Detection (DAD) being in progress.
    
    This commit implements the class NetworkAddressMonitor for Linux using
    netlink sockets. The FreeBSD implementation is subject to future work.
    
    A downside of the current implementation is that when a network is left,
    no Bye message can be sent. According attempts result in "bad file
    descriptor" error message on Linux. Thus, the host still appear in the
    network view of windows hosts. However, it did not work before anyways.
    
    The modified implementation also changes the behavior slightly. Previous
    to this commit, the program terminated at startup when no available
    address has been found. Now, the program continues assuming that a
    useable address will appear on any (specified) interface. However,
    specifying the -4 and -6 option still cause the program to terminate
    since no addresses will be useable anyways.
    
    see #25
    see #18, #29, #1

---
+++ test/netlink.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+# Not really a test case, but a PoC for getting notified about changes in
+# network addreses on Linux using netlink sockets.
+
+import os
+import socket
+import struct
+
+# from rtnetlink.h
+RTMGRP_LINK = 1
+RTMGRP_IPV4_IFADDR = 0x10
+RTMGRP_IPV6_IFADDR = 0x100
+
+RTM_NEWADDR = 20
+RTM_DELADDR = 21
+RTM_GETADDR = 22
+
+# from netlink.h
+NLM_F_REQUEST = 0x01
+
+NLM_F_ROOT = 0x100
+NLM_F_MATCH = 0x200
+NLM_F_DUMP = NLM_F_ROOT | NLM_F_MATCH
+
+# from if_addr.h
+IFA_ADDRESS = 1
+IFA_LOCAL = 2
+IFA_LABEL = 3
+IFA_FLAGS = 8
+
+# self_defines
+
+NLM_HDR_LEN = 16
+NLM_HDR_ALIGNTO = 4
+
+IFA_MSG_LEN = 8
+
+RTA_ALIGNTO = 4
+
+s = socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, socket.NETLINK_ROUTE)
+s.bind((os.getpid(), RTMGRP_LINK | RTMGRP_IPV4_IFADDR | RTMGRP_IPV6_IFADDR))
+
+kernel = (0, 0)
+req = struct.pack('@IHHIIB', NLM_HDR_LEN + 1, RTM_GETADDR, NLM_F_REQUEST |
+                  NLM_F_DUMP, 1, os.getpid(), socket.AF_PACKET)
+
+s.sendto(req, kernel)
+
+while True:
+    buf, src = s.recvfrom(4096)
+
+    offset = 0
+    while offset < len(buf):
+        (h_len, h_type, h_flags, _, _) = struct.unpack_from(
+            '@IHHII', buf, offset)
+
+        msg_len = h_len - NLM_HDR_LEN
+        if msg_len < 0:
+            # print('invalid message size')
+            break
+
+        if h_type != RTM_NEWADDR and h_type != RTM_DELADDR:
+            offset += ((msg_len + 1) // NLM_HDR_ALIGNTO) * NLM_HDR_ALIGNTO
+            # print('not interested in message type ', h_type)
+            # print('new offset: ', offset)
+            continue
+
+        offset += NLM_HDR_LEN
+        # decode ifaddrmsg as in rtnetlink.h
+        ifa_family, _, ifa_flags, ifa_scope, ifa_idx = struct.unpack_from(
+                '@BBBBI', buf, offset)
+
+        ifa_name = ''
+        addr = ''
+        # look for some details in attributes
+        i = offset + IFA_MSG_LEN
+        while i - offset < msg_len:
+            attr_len, attr_type = struct.unpack_from('HH', buf, i)
+            if attr_type == IFA_LABEL:
+                ifa_name, = struct.unpack_from(str(attr_len - 4 - 1) + 's',
+                                               buf, i + 4)
+            elif attr_type == IFA_LOCAL and ifa_family == socket.AF_INET:
+                b = buf[i + 4:i + 4 + 4]
+                addr = socket.inet_ntop(socket.AF_INET, b)
+            elif attr_type == IFA_ADDRESS and ifa_family == socket.AF_INET6:
+                b = buf[i + 4:i + 4 + 16]
+                addr = socket.inet_ntop(socket.AF_INET6, b)
+            elif attr_type == IFA_FLAGS:
+                _, ifa_flags = struct.unpack_from('HI', buf, i)
+            i += ((attr_len + 1) // RTA_ALIGNTO) * RTA_ALIGNTO
+
+        msg_type = 'NEW' if h_type == RTM_NEWADDR else 'DEL'
+        print('{} addr on interface {} {} [{}]: {}'.format(msg_type, ifa_name,
+              ifa_idx, hex(ifa_flags), addr))
+
+        offset += ((msg_len + 1) // NLM_HDR_ALIGNTO) * NLM_HDR_ALIGNTO
