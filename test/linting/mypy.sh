ed669cef7abe10eb20607bfa98711116b6b8c72b
Steffen Christgau <mail@s14u.de>
2022-01-29 18:31:15 +0100

    feat(test): add shell script for type checking
    Note that currently, mymy/typeshed have an issue with NETLINK_ROUTE and
    an error is raised although this is valid attribute from the socket
    module. See https://github.com/python/mypy/issues/12097 for details. In
    the meantime, use mypy 0.910.

---
+++ test/linting/mypy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+root_dir="$(realpath $(dirname $0)/../..)"
+
+for version in 3.7 3.8 3.9 3.10; do
+	echo -n "checking for Python ${version}..."
+	mypy --python-version=${version} ${root_dir}/src/wsdd.py
+	echo
+done
