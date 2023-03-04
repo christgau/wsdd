48f4709a79989427588dab6755c3ef5460ead8a2
Steffen Christgau <mail@s14u.de>
2023-03-04 12:12:51 +0100

    feat(test): add check support for Python 3.11

--- test/linting/mypy.sh
+++ test/linting/mypy.sh
@@ -2,7 +2,7 @@
 
 root_dir="$(realpath $(dirname $0)/../..)"
 
-for version in 3.7 3.8 3.9 3.10; do
+for version in 3.7 3.8 3.9 3.10 3.11; do
 	echo -n "checking for Python ${version}..."
 	mypy --python-version=${version} ${root_dir}/src/wsdd.py
 	echo
